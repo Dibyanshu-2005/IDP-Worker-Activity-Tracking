9556e04dec9243eea0a8303fae27cebfc8b4f925
Dibyanshu-2005 <dashdibyanshu2005@gamil.com>
2026-02-08 16:30:38 +0530

    Removed unnecessary comments

--- inference.py
+++ inference.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 def run_inference(video_path):
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8n.pt") # yolo version 8 nano model
 
     model.predict(
         source=video_path,
-        show=True,      # 👈 LIVE DISPLAY WINDOW
+        show=True,      
         save=True,
         conf=0.25
     )
