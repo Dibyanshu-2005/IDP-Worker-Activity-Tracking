3f1c85d9b15e5801810ce3542b93c625895a8c73
Dibyanshu-2005 <dashdibyanshu2005@gamil.com>
2026-02-11 15:52:26 +0530

    I removed the comment

--- inference.py
+++ inference.py
@@ -3,11 +3,6 @@ from ultralytics import YOLO
 def run_inference(video_path):
     model = YOLO("yolov8l.pt") # Trying a better version 
     
-    '''
-    I tried yolov8 nano, medium and large, we have to select one, and understand 
-    the trade-off between accuracy and speed. Would be great if you can test with yolo11 family models.
-    '''
-
     model.predict(
         source=video_path,
         classes=[0],  # Only detect person class
